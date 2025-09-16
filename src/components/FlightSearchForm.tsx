04847b0763cc90f355ecc56fb0c1a3609408e3e8
franlanese <lanesefranco2@gmail.com>
2025-09-16 09:48:23 -0300

    cambios varios

--- src/components/FlightSearchForm.tsx
+++ src/components/FlightSearchForm.tsx
@@ -71,7 +71,7 @@ export default function FlightSearchForm() {
     <>
     <Card className="w-full shadow-2xl py-4">
       <CardHeader>
-        <CardTitle className="font-headline text-2xl">Encuentre Su Vuelo Privado</CardTitle>
+        <CardTitle className="font-headline text-2xl">Encuentre su vuelo privado</CardTitle>
         <CardDescription>Ingrese los detalles de su viaje para comenzar.</CardDescription>
       </CardHeader>
       <CardContent>
